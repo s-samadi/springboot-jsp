ab5aac241cb8b1cc6856a0feb8e11241458ae0e4
Shadi Samadi <68650974+s-samadi@users.noreply.github.com>
2021-06-11 20:28:25 +1000

    Update HomeController.java

--- src/main/java/com/luwojtaszek/springbootjsp/web/controller/HomeController.java
+++ src/main/java/com/luwojtaszek/springbootjsp/web/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RequestParam; 
-
+import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by lukasz on 27.08.2017.
  * With IntelliJ IDEA 15
